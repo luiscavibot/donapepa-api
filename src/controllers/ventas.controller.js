25d511b67c5213170d2b9a28787e360c5bfad5d5
Luis Castillo <lcastillov123@gmail.com>
2021-02-01 12:23:00 -0500

    nueva url de api

--- src/controllers/ventas.controller.js
+++ src/controllers/ventas.controller.js
@@ -3,8 +3,8 @@ import axios from "axios";
 import { response } from "express";
 // import { getPagination } from "../libs/getPagination";
 
-const tokenNubeFactEmsion = "1b4c96ce6f1d4e9e9430a9b521bc21ed51dfdb1a9ddf42238e98b25b3fea6009";
-const apiUrlNubeFactEmsion = "https://api.nubefact.com/api/v1/8c7f8647-d998-470a-a9b2-13ebdc4b1150";
+const tokenNubeFactEmsion = "7a6189b3debb4677b13b5ade5b4ccbf664e9cf15e03149ca848d9aaa2770def1";
+const apiUrlNubeFactEmsion = "https://api.nubefact.com/api/v1/8e5b9739-af50-4eb9-911b-678592a8a0eb";
 
 export const crearVenta = async (req, res) => {
           const config = {
@@ -211,88 +211,6 @@ export const borrarTodasVentas = async (req, res) => {
     res.status(500).json({
       message:
         error.message || "Some error ocurred while removing all tutorials",
-    });
-  }
-};
-
-export const enviarBorrador = async (req, res) => {
-  try {
-      
-    // create tutorial
-    const venta = new Venta(
-      { 
-        usuario: req.body.usuario,
-        operacion: req.body.operacion,
-        tipo_de_comprobante: req.body.tipo_de_comprobante,
-        serie: req.body.serie,
-        numero:req.body.numero,
-        sunat_transaction: req.body.sunat_transaction,
-        cliente_tipo_de_documento: req.body.cliente_tipo_de_documento,
-        cliente_numero_de_documento: req.body.cliente_numero_de_documento,
-        cliente_denominacion: req.body.cliente_denominacion,
-        cliente_direccion: req.body.cliente_direccion,
-        cliente_email: req.body.cliente_email,
-        cliente_email_1: req.body.cliente_email_1,
-        cliente_email_2: req.body.cliente_email_2,
-        fecha_de_emision: req.body.fecha_de_emision,
-        fecha_de_vencimiento: req.body.fecha_de_vencimiento,
-        moneda: req.body.moneda,
-        tipo_de_cambio: req.body.tipo_de_cambio,
-        porcentaje_de_igv: req.body.porcentaje_de_igv,
-        descuento_global: req.body.descuento_global,
-        total_descuento: req.body.total_descuento,
-        total_anticipo: req.body.total_anticipo,
-        total_gravada: req.body.total_gravada,
-        total_inafecta: req.body.total_inafecta,
-        total_exonerada: req.body.total_exonerada,
-        total_igv: req.body.total_igv,
-        total_gratuita: req.body.total_gratuita,
-        total_otros_cargos: req.body.total_otros_cargos,
-        total: req.body.total,
-        percepcion_tipo: req.body.percepcion_tipo,
-        percepcion_base_imponible: req.body.percepcion_base_imponible,
-        total_percepcion: req.body.total_percepcion,
-        total_incluido_percepcion: req.body.total_incluido_percepcion,
-        total_impuestos_bolsas: req.body.total_impuestos_bolsas,
-        detraccion: req.body.detraccion,
-        observaciones: req.body.observaciones,
-        documento_que_se_modifica_tipo: req.body.documento_que_se_modifica_tipo,
-        documento_que_se_modifica_serie: req.body.documento_que_se_modifica_serie,
-        documento_que_se_modifica_numero: req.body.documento_que_se_modifica_numero,
-        tipo_de_nota_de_credito: req.body.tipo_de_nota_de_credito,
-        tipo_de_nota_de_debito: req.body.tipo_de_nota_de_debito,
-        enviar_automaticamente_a_la_sunat: req.body.enviar_automaticamente_a_la_sunat,
-        enviar_automaticamente_al_cliente: req.body.enviar_automaticamente_al_cliente,
-        condiciones_de_pago: req.body.condiciones_de_pago,
-        medio_de_pago: req.body.medio_de_pago,
-        placa_vehiculo: req.body.placa_vehiculo,
-        orden_compra_servicio: req.body.orden_compra_servicio,  
-        formato_de_pdf: req.body.formato_de_pdf,
-        generado_por_contingencia: req.body.generado_por_contingencia,
-        bienes_region_selva: req.body.bienes_region_selva,
-        servicios_region_selva: req.body.servicios_region_selva,
-        items: req.body.items,
-        //****VALORES EXTRA */
-        tipoRegalo: req.body.tipoRegalo,
-        cantidadRegalo: req.body.cantidadRegalo,
-        celular:req.body.celular,
-        numeroOperacion:req.body.numeroOperacion,  
-        provincia: req.body.provincia,
-        canalVenta: req.body.canalVenta,
-        clienteReferencias: req.body.clienteReferencias,
-        clienteDepartamento: req.body.clienteDepartamento,
-        clienteProvincia: req.body.clienteProvincia
-      }                
-    );
-
-    // Saving the task in the Database
-    const productoSaved = await venta.save();
-
-    // Responding to the client
-    return res.json(productoSaved);
-  } catch (error) {
-    res.status(500).json({
-      message: error.message || "Something went wrong creating the Task",
     });
   }
 };
